0501860997e1d19f2e529abee5e80a606ad7c1fa
lokhno <x_hunter@mail.ru>
2023-03-01 10:58:02 +0300

    3 варианта вывода решения

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,7 @@
 ﻿
-int number = 4;
-number = number * number;
-Console.WriteLine(number);
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("Your result is " + result);
+string second = "результат";
+Console.WriteLine("Your {1} is  {0}", result, second);
+Console.WriteLine($"Your result is {number*number}");
