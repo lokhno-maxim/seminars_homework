11d76faee411f9ce9d8527153be1a445bf6a7e2c
lokhno <x_hunter@mail.ru>
2023-03-01 11:08:54 +0300

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿﻿Console.WriteLine ("Введите ваше первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine ("Введите ваше второе число");
+int number2 = Convert.ToInt32(Console.ReadLine())
+
+
+string second = "результат";
+Console.WriteLine("Your {1} is  {0}", result, second);
