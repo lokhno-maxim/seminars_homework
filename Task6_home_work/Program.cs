2c89978637cd8e2ad01e86cbfcd3b0fb110fd2eb
lokhno <x_hunter@mail.ru>
2023-03-05 14:55:23 +0300

    finish work

--- Task6_home_work/Program.cs
+++ Task6_home_work/Program.cs
@@ -3,11 +3,15 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 ﻿Console.WriteLine ("Введите ваше второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
-if (max > number2);
-{
-    Console.Writeline ("максимальное из двух",(max));
- else
- {
-    Console.Writeline ("максимальное из двух",(number2));  
+
+if (max > number2)
+ { 
+  Console.WriteLine ("максимальное из двух");
+  Console.WriteLine(max);
+ } 
+else if(max < number2)
+ { 
+  Console.WriteLine ("максимальное из двух");  
+  Console.WriteLine(number2);
  }
-}
+
