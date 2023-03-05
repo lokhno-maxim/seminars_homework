f2ac3043f168e95dac8485306c0edddfa1d910c9
lokhno <x_hunter@mail.ru>
2023-03-05 15:48:53 +0300

    finish task

---
+++ Task5_home_work/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите ваше число");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+
+for (int i=1; number<=N; i++)
+{
+Console.WriteLine(number);
+number = number +2;
+}
