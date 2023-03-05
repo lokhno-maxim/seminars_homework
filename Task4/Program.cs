d0e8855543c6bbf63198477603e4a5da53a6b696
lokhno <x_hunter@mail.ru>
2023-03-05 13:57:57 +0300

    finish commit

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,7 +1,7 @@
 ﻿﻿Console.WriteLine ("Введите ваше число");
 int N = Convert.ToInt32(Console.ReadLine());
-int number = N;
-for (int i=1; i<=N; i++)
+int number = N*(-1);
+for (int i=N*(-1); i<=N; i++)
 {
 Console.WriteLine(number);
 number++;
