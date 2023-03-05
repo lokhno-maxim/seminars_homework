ddde1a3b2b5e4a627e28d3bfd560afb0b33a3b3e
lokhno <x_hunter@mail.ru>
2023-03-05 15:40:54 +0300

    finish task

---
+++ Task4_home_work/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ostatok;
+ostatok = number%2;
+switch (ostatok)
+{
+case 1:
+   Console.WriteLine("Число нечетное");
+   break;
+
+case 0:
+   Console.WriteLine("Число четное");
+   break;
+}
