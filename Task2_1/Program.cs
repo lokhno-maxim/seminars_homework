6a704f7795ca197c8b629531bde1756cd7915276
lokhno <x_hunter@mail.ru>
2023-03-05 20:18:11 +0300

    initial commit

---
+++ Task2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Random rand = new Random();
+int number = rand.Next(10,100);
+Console.WriteLine(number);
+int max = number / 10;
+int min = number % 10;
+if (max > min)
+Console.WriteLine("Наибольшее цифра числа это {0}", max);
+else Console.WriteLine("Наибольшее цифра числа это {0}", min);
