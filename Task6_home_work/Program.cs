dfa2b39300873552b1fb7cabb256b50c9db5203a
lokhno <x_hunter@mail.ru>
2023-03-05 14:37:51 +0300

    initial commit

---
+++ Task6_home_work/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿Console.WriteLine ("Введите ваше первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine ("Введите ваше второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max > number2);
+{
+    Console.Writeline ("максимальное из двух",(max));
+ else
+ {
+    Console.Writeline ("максимальное из двух",(number2));  
+ }
+}
