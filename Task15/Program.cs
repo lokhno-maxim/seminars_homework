31823d4625da8411cde269335be17cfc6d176566
lokhno <x_hunter@mail.ru>
2023-03-13 20:26:00 +0300

    начинаем писать

---
+++ Task15/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine ("Введите ваше число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+switch (number1) 
+{
+  case 1:
+   Console.WriteLine("НЕТ.Понедельник это будний день.");
+   break;
+  
+  case 2:
+   Console.WriteLine("НЕТ.Вторник это будний день.");
+  break;
+  
+  case 3:
+   Console.WriteLine("НЕТ.Среда это будний день.");
+  break;
+
+  case 4:
+   Console.WriteLine("НЕТ.Четверг это будний день.");
+  break;
+
+  case 5:
+   Console.WriteLine("НЕТ.Пятница это будний день.");
+  break;
+
+  case 6:
+   Console.WriteLine("ДА.Суббота это выходной день.");
+  break;
+
+  case 7:
+   Console.WriteLine("ДА.Воскресенье это выодной день.");
+  break;
+
+  default:
+   Console.WriteLine("Incorrect version");
+break;
+}
