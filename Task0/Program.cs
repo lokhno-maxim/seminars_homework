e4dae39312f5fd48cfab45e16f052aae79446681
lokhno <x_hunter@mail.ru>
2023-03-13 20:45:29 +0300

    first try

--- Task0/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Random rand = new Random();
-int number = rand.Next(100, 1000);
-int first_number = number / 100;
-int second_number = number % 100;
-second_number = second_number % 10;
-int result = 10 * first_number + second_number;
-Console.WriteLine(number);
-Console.WriteLine(result);
-
---
+++ Task14/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите два числа. Первое");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе.");
+int second_number = Convert.ToInt32(Console.ReadLine());
+int result = first_number % second_number;
+if (result == 0)
+Console.WriteLine("Первоче число {0} кратно второму {1}", first_number , second_number);
+else Console.WriteLine("Числа {0} и {1} не кратны", first_number , second_number);
