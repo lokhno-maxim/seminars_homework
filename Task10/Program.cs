d707644a0a69ed68d4caaa707584dd6991d4a0a7
lokhno <x_hunter@mail.ru>
2023-03-13 18:44:46 +0300

    первый заход

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите ваше число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int number_ostatok = N % 100;
+int second_number = number_ostatok / 10;
+
+Console.WriteLine(second_number);
