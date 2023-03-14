bf4e498562e3d039a08edad9520b82a889adb6e3
lokhno <x_hunter@mail.ru>
2023-03-14 12:35:21 +0300

    Работает с пятизначными цыфрами

---
+++ Task13/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine ("Введите ваше число");
+int number = Convert.ToInt32(Console.ReadLine());
+int proverka = number / 100;
+
+if (proverka >= 1 && proverka <=10000)
+{   if (proverka >= 1 && proverka <=10)
+    
+    {
+        int result = number % 100;
+        result = result % 10;
+        Console.WriteLine("Число трёхзначное. 3я цыфра {0}.", result);
+    }
+
+    if (proverka >=11 && proverka <= 99)
+
+    { 
+        int result = number % 1000;
+        result = result % 100;
+        result = result / 10;
+        Console.WriteLine("Число четырёхзначное. 3я цыфра {0}.", result);
+    }
+
+    if (proverka >=100 && proverka <= 999)
+
+    {   int result = number % 10000;
+        result = result % 1000;
+        result = result / 100;
+        Console.WriteLine("Число пятизначное. 3я цыфра {0}.", result);
+    }
+
+    if (proverka >= 1000) Console.WriteLine("Программа не предназначена для обработки таких больших чисел.");
+}
+else if (proverka == 0) Console.WriteLine("Число меньше 100");
+
--- Task14/Program.cs
+++ Task14/Program.cs
@@ -6,3 +6,4 @@ int result = first_number % second_number;
 if (result == 0)
 Console.WriteLine("Первоче число {0} кратно второму {1}", first_number , second_number);
 else Console.WriteLine("Числа {0} и {1} не кратны", first_number , second_number);
+// string result = firstNumber % secondNumber == 0 ? "Второе число кратно первому": $ "Второе число не кратно первому. Остаток от деления = {FirstNumber%SecondNumber}";
