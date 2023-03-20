1249edb919e832ca52ce59c0d883d2391868278d
lokhno <x_hunter@mail.ru>
2023-03-20 14:57:22 +0300

    working version

---
+++ Task11/Program.cs
@@ -0,0 +1,9 @@
+﻿Random rand = new Random();
+int number = rand.Next(100, 1000);
+int first_number = number / 100;
+int second_number = number % 100;
+second_number = second_number % 10;
+int result = 10 * first_number + second_number;
+Console.WriteLine(number);
+Console.WriteLine(result);
+
