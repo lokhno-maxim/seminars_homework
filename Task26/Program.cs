11a386c303ef73d09cc0d29e5a45f0aa6025ddbe
lokhno <x_hunter@mail.ru>
2023-03-21 12:58:30 +0300

    trying another folder

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿int Prompt()
+{
+ Console.WriteLine ("Введите число");
+ return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNums(int number)
+{
+    int i = 1;
+    while (number /10!=0);
+    {
+        number = number / 10;
+        i = i + 1 ;
+    } 
+      return i;
+}
+int a = Prompt();
+Console.WriteLine(SumNums(a));
+    
