5d40c8bac49f10f51c58f2e92585c33d7bc068f8
lokhno <x_hunter@mail.ru>
2023-03-21 20:58:15 +0300

    С первого раза!

---
+++ Task34/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите длинну массива");
+int array_str = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[array_str];
+for (int i = 0; i < array_str; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+
+Console.Write("[ ");
+for (int i = 0; i < array_str; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.Write("]");
+Console.WriteLine(" ");
+int j = 0;
+for (int i = 0; i < array_str; i++)
+{
+    if (array[i] % 2 == 0) j++;
+}
+
+Console.WriteLine(" ");
+if (j > 0) Console.WriteLine("Количество четных чисел " + j);
+else Console.WriteLine("Четных чисел не обнаружено");
