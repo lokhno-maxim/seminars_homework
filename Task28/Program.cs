c9185fd6d218dfc55db79c4a5c87498d6ae4cb69
lokhno <x_hunter@mail.ru>
2023-03-21 14:18:34 +0300

    working

---
+++ Task28/Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt()
+{
+ Console.WriteLine ("Введите число");
+ return Convert.ToInt32(Console.ReadLine());
+}
+int result = 1;
+int Nums(int number)
+{
+ int i; 
+    for (i = 1; i <= number; i++)
+    {
+      result = result*i;
+    }
+    return result;
+}
+int a = Prompt();
+Console.WriteLine(Nums(a));
