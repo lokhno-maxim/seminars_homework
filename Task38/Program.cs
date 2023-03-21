4b6fa6bc46648452787222fab3ec4b049d1046e7
lokhno <x_hunter@mail.ru>
2023-03-21 21:12:43 +0300

    очень быстро очень вместе

---
+++ Task38/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите длинну массива");
+int array_str = Convert.ToInt32(Console.ReadLine());
+
+float[] array = new float[array_str];
+for (int i = 0; i < array_str; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+
+Console.Write("[ ");
+for (int i = 0; i < array_str; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.Write("]");
+Console.WriteLine(" ");
+float min; float max;
+float result = 0;
+min = max = array[0];
+for (int i = 0; i < array_str; i++)
+{
+   if (min > array[i]) min = array[i];
+   if (max < array[i]) max = array[i];
+    
+}
+result = max - min;
+Console.WriteLine(" ");
+Console.WriteLine("Разность между max && min = " + result);
