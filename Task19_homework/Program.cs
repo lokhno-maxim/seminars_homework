d42b95633604a74a00560ea24f1d56d7cb0e27c8
lokhno <x_hunter@mail.ru>
2023-03-21 13:00:11 +0300

    trying another folder homework

---
+++ Task19_homework/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите 5_значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [5];  
+int work_number = number;
+int i;
+  if (number > 9999 && number < 100000)  
+    {
+      for (i = 0 ; i < array.Length ; i ++)
+     {
+      work_number = number % 10;
+      array[i] = work_number;
+      number /= 10;
+     }
+     if (array[0] == array[4] && array[1] == array[3]) Console.WriteLine("Полиндром");
+     else Console.WriteLine("НЕТ");
+    }
+  else Console.WriteLine("Incorrect number");
