f8f38b19dae5285fcae4c52d6fa1845c504b0670
lokhno <x_hunter@mail.ru>
2023-03-21 21:04:47 +0300

    second try

---
+++ Task36/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите длинну массива");
+int array_str = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[array_str];
+for (int i = 0; i < array_str; i++)
+{
+    array[i] = new Random().Next(-99, 100);
+}
+
+Console.Write("[ ");
+for (int i = 0; i < array_str; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.Write("]");
+Console.WriteLine(" ");
+
+int result = 0;
+for (int i = 0; i < array_str; i=i+2)
+{
+    result = array[i] + result;
+    
+}
+
+Console.WriteLine(" ");
+Console.WriteLine("Сумма элементов на нечетных позициях " + result);
