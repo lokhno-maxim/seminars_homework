dfe47be598151afeab0bb3a74f5cc9f08a95ce01
lokhno <x_hunter@mail.ru>
2023-03-21 20:29:20 +0300

    заработало

---
+++ Task35/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = new int[123];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-999, 1000);
+}
+
+Console.Write("[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+Console.Write("]");
+Console.WriteLine(" ");
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i] <= 99)
+    j++;
+}
+
+Console.WriteLine(" ");
+if (j > 0) Console.WriteLine("Мы его нашли " + j + " раз");
+else Console.WriteLine("Его там нет");
