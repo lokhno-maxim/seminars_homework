ed5c84fbf1d7608224bf8588b45cc53ff89f6bae
lokhno <x_hunter@mail.ru>
2023-03-21 16:20:40 +0300

    складывает

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿int Prompt()
+{
+ Console.WriteLine ("Введите число");
+ return Convert.ToInt32(Console.ReadLine());
+}
+int number;
+int work_number;
+int result; result = 0;
+
+number = Prompt();
+
+while (number > 0)
+ {
+    work_number = number % 10;
+    result = result + work_number;
+    number /= 10;
+ }
+     
+
+Console.WriteLine(result);     
+
+ 
