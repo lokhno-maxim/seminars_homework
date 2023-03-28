06371cc4186393cef088f9b168f0f245d2688237
lokhno <x_hunter@mail.ru>
2023-03-28 10:33:12 +0300

    first try

---
+++ Task40/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает 3 числа и проверяет могут ли они образовать треугольник.
+
+Console.WriteLine("Введите длинну стороны а =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длинну стороны b =");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длинну стороны c =");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b+c && b < a+c && c < b+a) Console.WriteLine("Существует вероятность того, что данные длинны сторон могут образовать общий треугольник");
+ else Console.WriteLine("НЕТ");
+ 
