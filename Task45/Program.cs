8be71149222356ac22413137bcbb43e5c12d2b6c
lokhno <x_hunter@mail.ru>
2023-03-30 10:26:43 +0300

    first try

---
+++ Task45/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите длинну массива");
+int array_str = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[array_str];
+int [] copied_array = new int [array_str];
+for (int i = 0; i < array_str; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+
+Console.Write("First array = [ ");
+for (int i = 0; i < array_str; i++)
+{
+    Console.Write(array[i] + ", ");
+    copied_array[i] = array[i];
+}
+Console.Write("]");
+Console.WriteLine(" ");
+
+
+Console.Write("Copied array = [ ");
+for (int i = 0; i < array_str; i++)
+{
+    Console.Write(copied_array[i] + ", ");
+}
+Console.Write("]");
+Console.WriteLine(" ");
