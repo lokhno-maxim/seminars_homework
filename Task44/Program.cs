b00216353637fed76adae050de14eb7ac1cc368e
lokhno <x_hunter@mail.ru>
2023-03-30 10:05:56 +0300

    рабочая версия

---
+++ Task44/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("Введите число N=");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+array[0] = 0; array[1] = 1;
+int tmp; tmp = array[0];
+
+
+for (int i = 2; i < n; i++)
+{
+    tmp = array[i - 2] + array[i - 1];
+    array[i] = tmp;
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(array[i] + " ");
+}
